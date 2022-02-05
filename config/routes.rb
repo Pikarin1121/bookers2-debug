d1b2049c8db28510fb0dcec9ddfc40d799e48f68
EC2 Default User <ec2-user@ip-172-31-85-127.ec2.internal>
2022-02-05 06:12:42 +0000

    [add]検索機能

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
+  get "search" => "searches#search"
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resources :book_comments, only: [:create,:destroy]
@@ -15,5 +16,6 @@ Rails.application.routes.draw do
     get 'followings', on: :member
     get 'followers' , on: :member
   end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
