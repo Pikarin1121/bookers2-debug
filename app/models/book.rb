bdf00e64a02063a7fee8cc9a4f7215be13e1d584
EC2 Default User <ec2-user@ip-172-31-85-127.ec2.internal>
2022-02-10 10:43:59 +0000

    [add]いいね多い順

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,7 @@ class Book < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   has_many :favorites, dependent: :destroy
+  has_many :favorited_users, through: :favorites, source: :user
   has_many :book_comments, dependent: :destroy
 
   validates :title,presence:true
@@ -18,7 +19,7 @@ class Book < ApplicationRecord
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
   end
-  
+
   def self.looks(search, word)
     if search == "perfect_match"
       @book = Book.where("title LIKE?","#{word}")
